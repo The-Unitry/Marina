69cc01d4e3aae8542b09351d16bb43e0c30548e1
Allan de Wit <allandewit@live.nl>
2016-05-19 11:34:28 +0200

    Added User class

---
+++ Navicula/app/src/main/java/eu/theunitry/navicula/User.java
@@ -0,0 +1,99 @@
+package eu.theunitry.navicula;
+
+/**
+ * Created by Allan on 19-5-2016.
+ */
+public class User {
+
+    private int id;
+    private String email;
+    private String firstname;
+    private String insertion;
+    private String surname;
+    private String phoneNumber;
+    private String address;
+    private String zipcode;
+    private String city;
+    private int role;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getInsertion() {
+        return this.insertion;
+    }
+
+    public void setInsertion(String insertion) {
+        this.insertion = insertion;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return this.zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getRole() {
+        return this.role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+}
+
