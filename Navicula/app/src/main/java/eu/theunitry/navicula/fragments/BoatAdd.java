9c818fbf89c3b956cf3bd4dd39856a68bbbfdfa4
JBronkhorst <j.bronkhorst98@gmail.com>
2016-05-26 10:18:57 +0200

    Updated boat Add layout. Added spinner functionalities.

--- Navicula/app/src/main/java/eu/theunitry/navicula/fragments/BoatAdd.java
+++ Navicula/app/src/main/java/eu/theunitry/navicula/fragments/BoatAdd.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import eu.theunitry.navicula.FragmentMain;
 import eu.theunitry.navicula.R;
@@ -12,7 +14,17 @@ public class BoatAdd extends FragmentMain {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
         return inflater.inflate(R.layout.fragment_boat_add, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinnerBoatColor);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.boat_add_boat_color, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
