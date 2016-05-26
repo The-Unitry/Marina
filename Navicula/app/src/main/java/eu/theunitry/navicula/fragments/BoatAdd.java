d42559ad79baaa1543d00a5193ba4071ceebc835
JBronkhorst <j.bronkhorst98@gmail.com>
2016-05-26 11:14:49 +0200

    Updated boat Add layout. Added extra Boat type Radio Button.

--- Navicula/app/src/main/java/eu/theunitry/navicula/fragments/BoatAdd.java
+++ Navicula/app/src/main/java/eu/theunitry/navicula/fragments/BoatAdd.java
@@ -1,16 +1,20 @@
 package eu.theunitry.navicula.fragments;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import eu.theunitry.navicula.FragmentMain;
 import eu.theunitry.navicula.R;
 
 public class BoatAdd extends FragmentMain {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -27,4 +31,5 @@ public class BoatAdd extends FragmentMain {
         spinner.setAdapter(adapter);
 
     }
+
 }
