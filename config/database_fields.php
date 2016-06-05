3d053cba1a6b7828efc7f5adc3c610614599a222
Maarten <maartenbodepc@gmail.com>
2016-06-05 19:47:32 +0200

    Move role fields to database_fields config file

---
+++ config/database_fields.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'roles' => [
+        [
+            'id' => 1,
+            'name' => 'superadmin'
+        ],
+        [
+            'id' => 2,
+            'name' => 'admin'
+        ],
+        [
+            'id' => 3,
+            'name' => 'manager'
+        ],
+        [
+            'id' => 4,
+            'name' => 'user'
+        ],
+        [
+            'id' => 5,
+            'name' => 'visitor'
+        ]
+    ]
+];
