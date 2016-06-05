a3aad573c60eb0441f61943902fa322aa30ceab9
Maarten <maartenbodepc@gmail.com>
2016-06-05 19:54:30 +0200

    Add start and end dates to reservations overview

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -12,6 +12,7 @@
                 <th>#</th>
                 <th>Requester</th>
                 <th>Start date</th>
+                <th>End date</th>
                 <th></th>
             </tr>
             </thead>
@@ -25,7 +26,10 @@
                         {{ $reservation->requester->name }}
                     </td>
                     <td>
-                        {{ ucfirst($reservation->role->name) }}
+                        {{ $reservation->start }}
+                    </td>
+                    <td>
+                        {{ $reservation->end }}
                     </td>
                     <td>
                         <a href="/admin/user/{{ $reservation->id }}">View</a>
