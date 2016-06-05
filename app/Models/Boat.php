5f50c73060294ebe2d52858d54a9b26a4ea4b00e
Maarten <maartenbodepc@gmail.com>
2016-06-05 16:29:40 +0200

    Create owner relation to Boat model

--- app/Models/Boat.php
+++ app/Models/Boat.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Boat extends Model
 {
-    //
+    /**
+     * Get the owner of the boat.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
