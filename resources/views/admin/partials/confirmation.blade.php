0757196fd3e8b2ae5c8bb8023238edd82ef23edf
Maarten <maartenbodepc@gmail.com>
2016-06-06 23:51:07 +0200

    Fix styling bug where alert would get under content

--- resources/views/admin/partials/confirmation.blade.php
+++ resources/views/admin/partials/confirmation.blade.php
@@ -1,6 +1,5 @@
 @if(Session::has('message'))
     <div class="alert alert-success" id="alert" role="alert">
-        <button type="button" class="close" data-dismiss="alert">x</button>
         <span>{{ Session::get('message') }}</span>
     </div>
 @endif
