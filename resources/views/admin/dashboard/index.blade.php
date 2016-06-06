c13338530f161e3debb2c28de1756d7a3669b164
Maarten <maartenbodepc@gmail.com>
2016-06-06 23:48:28 +0200

    Add jumbotron to dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,4 +5,8 @@
 @endsection
 
 @section('content')
+    <div class="jumbotron">
+        <h1>Navicula</h1>
+        <p style="font-weight: 300;">Harbour management made easy.</p>
+    </div>
 @endsection
