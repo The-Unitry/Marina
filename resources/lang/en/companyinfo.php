8cf58f045be485c3292417110f98774f973b2267
JBronkhorst <j.bronkhorst98@gmail.com>
2016-06-08 13:57:55 +0200

    Add translation structure to view

---
+++ resources/lang/en/companyinfo.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Company info Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This is the text used for company info in the app.
+    |
+    */
+
+    'company_name' => 'Company name',
+    'company_email' => 'Company email',
+    'company_address' => 'Company address',
+    'company_zipcode' => 'Company zipcode',
+    'company_city' => 'Company city',
+    'company_phone' => 'Company phone number',
+    'company_tax' => 'Company tax',
+    'company_tourist_tax' => 'Company tourist tax',
+    'company_logo' => 'Company logo',
+
+];
--- resources/lang/en/invoice.php
+++ resources/lang/en/invoice.php
@@ -7,21 +7,16 @@ return [
     | Invoice Language Lines
     |--------------------------------------------------------------------------
     |
-    | This is the text used for the invoice
+    | This is the text used for invoice lines in the app.
     |
     */
 
     'thank_you_reservation' => 'Thank you for your reservation.',
-    'box' => 'Box',
-    'start_date' => 'Start date',
-    'end_date' => 'End date',
     'amount_of_nights' => 'Amount of nights',
     'total_price' => 'Total price',
     'receive_payment_within_days' => 'We would like to receive your payment within :days days on bank account: :iban in attn. of ' ,
     'sign_off' => 'Yours sincerely,',
-
     'customer' => 'Customer',
     'price' => 'Price',
-    'status' => 'Status',
 
 ];
