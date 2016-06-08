e9889b683f2a306c6cb1c761f563cc1c85cf054e
JBronkhorst <j.bronkhorst98@gmail.com>
2016-06-08 11:26:07 +0200

    Add more translations to view

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -12,55 +12,55 @@
                     {{ method_field($method) }}
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label for="company_name" class="col-sm-2 control-label">Company name</label>
+                        <label for="company_name" class="col-sm-2 control-label">{{ trans('columns.company_name') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="company_name" id="company_name" value="{{ setting('company_name') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="company_mail" class="col-sm-2 control-label">Company mail</label>
+                        <label for="company_mail" class="col-sm-2 control-label">{{ trans('columns.company_mail') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="company_mail" id="company_mail" value="{{ setting('company_mail') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="company_address" class="col-sm-2 control-label">Company address</label>
+                        <label for="company_address" class="col-sm-2 control-label">{{ trans('columns.company_address') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="company_address" id="company_address" value="{{ setting('company_address') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="company_zipcode" class="col-sm-2 control-label">Company zipcode</label>
+                        <label for="company_zipcode" class="col-sm-2 control-label">{{ trans('columns.company_zipcode') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="company_zipcode" id="company_zipcode" value="{{ setting('company_zipcode') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="company_city" class="col-sm-2 control-label">Company city</label>
+                        <label for="company_city" class="col-sm-2 control-label">{{ trans('columns.company_city) }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="company_city" id="company_city" value="{{ setting('company_city') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="company_phone" class="col-sm-2 control-label">Company phone</label>
+                        <label for="company_phone" class="col-sm-2 control-label">{{ trans('columns.company_phone') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="company_phone" id="company_phone" value="{{ setting('company_phone') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="tax" class="col-sm-2 control-label">Tax (%)</label>
+                        <label for="tax" class="col-sm-2 control-label">{{ trans('columns.company_tax') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="tax" id="tax" value="{{ setting('tax') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="tourist_tax" class="col-sm-2 control-label">Tourist tax (cents)</label>
+                        <label for="tourist_tax" class="col-sm-2 control-label">{{ trans('columns.company_tourist_tax') }}</label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="tourist_tax" id="tourist_tax" value="{{ setting('tourist_tax') }}">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="logo" class="col-sm-2 control-label">Logo</label>
+                        <label for="logo" class="col-sm-2 control-label">{{ trans('columns.company_logo') }}</label>
                         <div class="col-sm-10">
                             <input type="file" class="form-control" name="logo" id="logo">
                         </div>
@@ -68,7 +68,7 @@
                 </div>
                 <div class="col-md-2">
                     <div class="list-group">
-                        <button type="submit" class="list-group-item bg-primary"><i class="fa fa-floppy-o" aria-hidden="true"></i> Save</button>
+                        <button type="submit" class="list-group-item bg-primary"><i class="fa fa-floppy-o" aria-hidden="true"></i> {{ trans('columns.send_save') }}</button>
                     </div>
                 </div>
             </div>
