10c8f8ec0004c522fe46a09f2b6a25754b3d9fe9
wesleymeyerwm <wesleymeyerwm@hotmail.com>
2016-06-08 15:40:44 +0200

    Update newsletter page

--- resources/views/admin/newsletters/index.blade.php
+++ resources/views/admin/newsletters/index.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.admin')
 
-@section('title')
-    {{ trans('navigation.newsletters') }}
-@endsection
-
 @section('content')
-<table class="table table-striped" id="datatable">
-    <thead>
-    <tr>
-        <th width="5%">#</th>
-        <th>{{ trans('columns.sender') }}</th>
-        <th>{{ trans('columns.subject') }}</th>
-        <th>{{ trans('columns.content') }}</th>
-        <th>{{ trans('columns.send_date') }}</th>
-    </tr>
-    </thead>
-</table>
+<div class="container content">
+    <h3>
+        {{ trans('navigation.send_newsletter') }}
+    </h3>
+    <br>
+    <div class="row">
+        <!-- @if(Session::has('newsletter'))
+            <div class="col-md-12">
+                <p>{{ session('newsletter') }}</p>
+            </div>
+        @else -->
+        <form action="/newsletter" class="form-horizontal col-md-12" role="form" method="POST">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="body" class="col-sm-2 control-label">Nieuwsbrief inhoud</label>
+                <div class="col-sm-10">
+                    <textarea class="form-control" rows="4" name="body" placeholder="{{ trans('newsletter.your_newsletter') }}"></textarea>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary pull-right">{{ trans('newsletter.send_newsletter') }}</button>
+        </form>
+        <!-- @endif -->
+
+    </div>
+</div>
 @endsection
