2ac69518067574ed3c17d2fd9e93cbc3c8a041ff
maartenbode <maartenbodepc@gmail.com>
2016-06-14 23:21:18 +0200

    WIP: Redesign dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -12,8 +12,7 @@
     <div class="row">
         <a href="/admin/user">
             <div class="col-md-3 col-sm-6">
-                <div class="widget-panel bg-pink">
-                    <i class="fa fa-globe"></i> 
+                <div class="widget-panel">
                     <h2 class="text-white">{{ $transients }}</h2>
                     <div class="text-white">{{ trans_choice('columns.transients', $transients) }}</div>
                 </div>
@@ -22,8 +21,7 @@
 
         <a href="/admin/user">
             <div class="col-md-3 col-sm-6">
-                <div class="widget-panel bg-yellow">
-                    <i class="fa fa-home"></i> 
+                <div class="widget-panel">
                     <h2 class="text-white">{{ $berth_holders }}</h2>
                     <div class="text-white">{{ trans_choice('columns.berth_holders', $berth_holders) }}</div>
                 </div>
@@ -32,8 +30,7 @@
 
         <a href="/admin/boat">
             <div class="col-md-3 col-sm-6">
-                <div class="widget-panel bg-blue">
-                    <i class="fa fa-ship"></i> 
+                <div class="widget-panel">
                     <h2 class="text-white">{{ $boats }}</h2>
                     <div class="text-white">{{ trans_choice('columns.boats', $boats) }}</div>
                 </div>
@@ -42,8 +39,7 @@
 
         <a href="/admin/user">
             <div class="col-md-3 col-sm-6">
-                <div class="widget-panel bg-green">
-                    <i class="fa fa-user"></i> 
+                <div class="widget-panel">
                     <h2 class="text-white">{{ $users }}</h2>
                     <div class="text-white">{{ trans_choice('columns.users', $users) }}</div>
                 </div>
@@ -51,17 +47,19 @@
         </a>
     </div>
     <style type="text/css">
-        .widget-panel {
-            -webkit-box-shadow: 0px 1px 2px 0px rgba(0,0,0,0.1);
-            -moz-box-shadow: 0px 1px 2px 0px rgba(0,0,0,0.1);
-            box-shadow: 0px 1px 2px 0px rgba(0,0,0,0.1);
-        }
 
         .widget-panel {
             padding: 40px 20px;
             border-radius: 4px;
             position: relative;
             margin-bottom: 20px;
+            background-color: #EEE;
+            color: #000000;
+            text-align: center;
+        }
+
+        .widget-panel .fa {
+            color: #cccccc;
         }
 
         .widget-panel h2 {
@@ -75,27 +73,6 @@
             font-size: 60px;
             right: 0;
             bottom: 10px;
-            color: #FFFFFF;
-        }
-
-        .bg-pink {
-            background-color: #F16189;
-        }
-
-        .bg-yellow {
-            background-color: #EDCB65;
-        }
-
-        .bg-blue {
-            background-color: #47B7E1;
-        }
-
-        .bg-green {
-            background-color: #59C487;
-        }
-
-        .text-white {
-            color: #FFFFFF;
         }
     </style>
 @endsection
