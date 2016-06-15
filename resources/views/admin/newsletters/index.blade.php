ed957c0aa7bbf8ca6f99a6f4c689548f86b4db41
Jelmer Portegijs <japortegijs@gmail.com>
2016-06-15 12:02:51 +0200

    Add confirmation window to newsletter

--- resources/views/admin/newsletters/index.blade.php
+++ resources/views/admin/newsletters/index.blade.php
@@ -28,8 +28,29 @@
                     <textarea class="form-control" rows="4" name="body"></textarea>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary pull-right">{{ trans('actions.send.newsletter') }}</button>
+            <a type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary pull-right">{{ trans('actions.send.newsletter') }}</a>
         </form>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title text-danger" id="myModalLabel"><strong>Let op!</strong></h3>
+            </div>
+            <div class="modal-body">
+                <h4>
+                    U staat op het punt een nieuwsbrief te sturen naar alle gebruikers! Wilt u doorgaan?
+                </h4>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Sluiten</button>
+                <a href="" class="btn btn-success">Verstuur</a>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
