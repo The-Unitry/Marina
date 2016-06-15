836c152cd06ef76a83ce4e5e575adef4ef741386
maartenbode <maartenbodepc@gmail.com>
2016-06-15 19:22:19 +0200

    Change document sharing phrasing

--- resources/views/admin/Documents/index.blade.php
+++ resources/views/admin/Documents/index.blade.php
@@ -7,10 +7,11 @@
 @section('content')
     <div class="row">
         <div class="col-md-10">
-            Om een bestand te delen met leden van de haven klik op het bestand en kopieer de link bovenaan de pagina. of klik voeg
-            <a href="https://drive.google.com/folderview?id=0B62kPmaCjokDVFBSdVVRZDczYlk&usp=sharing" target="_blank">deze</a> link toe
+            Om een bestand te delen met leden van de haven, klikt op een bestand en kopieert u de link uit de URL balk. Een nieuw document aanmaken kan
+            <a href="https://drive.google.com/folderview?id=0B62kPmaCjokDVFBSdVVRZDczYlk&usp=sharing" target="_blank">hier</a>.
         </div>
-        <div class="col-md-10">
+        <br><br>
+        <div class="col-md-12">
             <iframe class="frame frame-control" data-frameload="true" src="https://drive.google.com/embeddedfolderview?id=0B62kPmaCjokDVFBSdVVRZDczYlk#list" style="width:100%; height:500px; border:0;"></iframe>
         </div>
     </div>
